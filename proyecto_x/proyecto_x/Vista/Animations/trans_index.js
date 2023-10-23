c03d81379617866409dac048a09e83102337cd6d
Santiago Pereira <137733451+SantiPereira@users.noreply.github.com>
2023-10-23 00:27:40 -0300

    SISVIANSA EN SU VERCION V-2.2.2
    Se agrego la vista de subir menu y la de ver pedidos , ademas se arreglaron errores de otras vistas

---
+++ proyecto_x/proyecto_x/Vista/Animations/trans_index.js
@@ -0,0 +1,162 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween : 30,
+    loop: true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true, 
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+})
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween : 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        }
+    }
+})
+
+
+//Carrito - En un futuro se pasara esta funcionalidad a php 
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const elementos2 = document.getElementById('lista-2');
+const elementos3 = document.getElementById('lista-3');
+const lista = document.querySelector('#lista-carrito tbody');
+//Falta Impleentar
+//const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+    elementos1.addEventListener('click', comprarElemento);
+    elementos2.addEventListener('click', comprarElemento);
+    elementos3.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    //vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-ic')
+    }
+
+    insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100>
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a href="#" class="borrar" data-id="${elemento.id}">Eliminar</a>
+        </td>
+    `;
+
+    lista.appendChild(row);
+    actualizarTotal();
+}
+
+function eliminarElemento(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('borrar')) {
+        const elemento = e.target.parentElement.parentElement;
+        elemento.remove();
+        actualizarTotal();
+    }
+}
+
+function vaciarCarrito() {
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    actualizarTotal();
+    return false;
+}
+
+function actualizarTotal() {
+    // Aquí puedes implementar la lógica para calcular y mostrar el total del carrito.
+}
+
+
+/*Barra lateral*/
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+    //console.log('clik')
+    document.getElementById('sidebar').classList.toggle('active');
+    //console.log(document.getElementById('sidebar'))
+});
+
+
+
+
+/*Carrito*/
+const addToCart=document.querySelectorAll('[data-btn-action="add-btn-cart"]');
+
+const closeModal=document.querySelector('.jsModalClose');
+
+addToCart.forEach(btn => {
+    
+    btn.addEventListener('click',(event)=>{
+
+        const nameModal=event.target.getAttribute('data-modal');
+
+        const modal=document.querySelector(nameModal);
+
+        //abrimos el modal
+        modal.classList.add('active');
+
+    })
+
+});
+
+//CERRAR EL MODAL
+closeModal.addEventListener('click',(event)=>{
+    event.target.parentNode.parentNode.classList.remove('active');
+})
+
+//CERRAMOS MODAL CUANDO HACEMOS CLICK FUERA DEL CONTENDINO DEL MODAL
+window.onclick = (event)=>{
+    const modal=document.querySelector('.modal.active');
+
+    if(event.target == modal){
+        modal.classList.remove('active');
+    }
+}
